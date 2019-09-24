e33eb34e7251b9f23865ab63ea77144b50585430
Junjun Zhang <junjun.ca@gmail.com>
2019-09-23 22:44:23 -0400

    first pass of mutect2 tool

--- tools/gatk-mutect2/gatk-mutect2.cwl
+++ tools/gatk-mutect2/gatk-mutect2.cwl
@@ -1,3 +1,75 @@
 cwlVersion: v1.1
 class: CommandLineTool
 id: gatk-mutect2
+
+requirements:
+- class: ShellCommandRequirement
+- class: DockerRequirement
+  dockerPull: 'quay.io/icgc-argo/gatk-mutect2:gatk-mutect2.4.1.3.0-1'
+
+baseCommand: [ 'gatk-mutect2.py' ]
+
+inputs:
+  jvm_mem:
+    type: int?
+    inputBinding:
+      prefix: -m
+  ref_fa:
+    type: File
+    inputBinding:
+      prefix: -R
+  tumour_reads:
+    type: File
+    inputBinding:
+      prefix: --tumour-reads
+    secondaryFiles: ['.bai?', '.crai?']
+  normal_reads:
+    type: File?
+    inputBinding:
+      prefix: --normal-reads
+    secondaryFiles: ['.bai?', '.crai?']
+  germline_resource:
+    type: File?
+    inputBinding:
+      prefix: --germline-resource
+    secondaryFiles: ['.idx?', '.tbi?']
+  pon:
+    type: File?
+    inputBinding:
+      prefix: -pon
+    secondaryFiles: ['.idx?', '.tbi?']
+  intervals:
+    type: File?
+    inputBinding:
+      prefix: -L
+  output_vcf:
+    type: string
+    inputBinding:
+      prefix: -O
+  bam_output:
+    type: string
+    inputBinding:
+      prefix: --bam-output
+  f1r2_tar_gz:
+    type: string?
+    inputBinding:
+      prefix: --f1r2-tar-gz
+  m2_extra_args:  # placeholder for now
+    type: string?
+    inputBinding:
+      prefix: --extra-args
+
+outputs:
+  unfiltered_vcf:
+    type: File
+    outputBinding:
+      glob: $(inputs.output_vcf).vcf.gz
+    secondaryFiles: [.tbi]
+  bam_output:
+    type: [null, File]
+    outputBinding:
+      glob: $(inputs.bam_output)
+  f1r2_counts:
+    type: [null, File]
+    outputBinding:
+      glob: $(inputs.f1r2_tar_gz)
--- tools/gatk-mutect2/gatk-mutect2.py
+++ tools/gatk-mutect2/gatk-mutect2.py
@@ -1,1 +1,79 @@
 #!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='GATK Mutect2')
+
+    parser.add_argument('-j', '--jvm-mem', dest='jvm_mem', type=int,
+                        help='JVM max memory in MB', default=1000)
+    parser.add_argument('-R', dest='ref_fa', type=str,
+                        help='Reference genome file (eg, .fa)', required=True)
+    parser.add_argument('-t', '--tumour-reads', dest='tumour_reads', type=str,
+                        help='Tumour reads file, bam or cram', required=True)
+    parser.add_argument('-n', '--normal-reads', dest='normal_reads', type=str,
+                        help='Normal reads file, bam or cram')
+    parser.add_argument('-g', '--germline-resource', dest='germline_resource', type=str,
+                        help='Population VCF of germline sequencing containing allele fractions, af-only-gnomad.vcf.gz.')
+    parser.add_argument('-L', '--intervals', dest='intervals', type=str,
+                        help='One or more genomic intervals over which to operate')
+    parser.add_argument('-O', '--output-vcf', dest='output_vcf', type=str,
+                        help='Output file with only the sample name in it.', required=True)
+    parser.add_argument('-b', '--bam-output', dest='bam_output', type=str,
+                        help='File to which assembled haplotypes should be written')
+    parser.add_argument('-f', '--f1r2-tar-gz', dest='f1r2_tar_gz', type=str,
+                        help='collect F1R2 counts and output files into this tar.gz file')
+
+    args = parser.parse_args()
+
+    # TODO: valid arguments, output_vcf must end with 'vcf.gz', bam_output must end with '.bam'
+    # f1r2_tar_gz must end with '.tar.gz'
+
+    # TODO: get normal sample name, use it as -normal when invoke Mutect2
+    normal_cmd = ''
+    if normal_reads:
+        normal_sample_name = 'test-normal'
+        normal_cmd = '-I %s -normal %s' % (normal_reads, normal_sample_name)
+
+    cmd = 'gatk --java-options "-Xmx%sm" Mutect2 -R %s -O %s -I %s %s' % (
+            jvm_mem, ref_fa, output_vcf, tumour_reads, normal_cmd
+        )
+
+    if intervals:
+        cmd = cmd + ' -L %s' % intervals
+
+    if bam_output:
+        cmd = cmd + ' --bam-output %s' % bam_output
+
+    if f1r2_tar_gz:
+        cmd = cmd + ' --f1r2-tar-gz %s' % f1r2_tar_gz
+
+    stdout, stderr, p, success = '', '', None, True
+    try:
+        p = subprocess.Popen([cmd],
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             shell=True)
+        stdout, stderr = p.communicate()
+
+    except Exception as e:
+        print('Execution failed: %s' % e, file=sys.stderr)
+        success = False
+
+    if p and p.returncode != 0:
+        print('Execution failed, none zero code returned. Error: %s' % stderr.decode("utf-8"),
+            file=sys.stderr)
+        success = False
+
+    print(stdout.decode("utf-8"))
+    print(stderr.decode("utf-8"), file=sys.stderr)
+
+    if not success:
+        sys.exit(p.returncode if p.returncode else 1)
+
+if __name__ == "__main__":
+    main()
